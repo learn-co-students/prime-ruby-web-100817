54b269551c31ce2395404bc2129d677d508929c1
Ryan Mascolo <rjmascolo@gmail.com>
2017-09-22 17:22:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  range = 2...num
+  range.each do |number|
+    if num % number === 0
+      return false
+    end
+  end
+  return true
+end
+
+puts prime?(13)
