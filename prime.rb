299e0a88b49e5cab52553eabaab4ee1c17ebfb4c
flored27 <dflores@fandm.edu>
2017-09-26 22:19:28 -0400

    weird

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if (number <= 1)
+    return false
+  end
+  for d in 2...number
+    if (number % d) == 0
+      return false
+    end
+  end
+      return true
+    end
