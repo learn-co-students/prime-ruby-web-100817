3068eaebbaf5834e949a518532e4eedd760c3a73
Genevieve Costa <togenny@yahoo.com>
2017-09-29 18:36:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+  srt = 2
+  if int < 2
+    return false
+  end
+  int_array = [*srt..int]
+  int_array.each do |n|
+    if (int % n == 0) && (int != n)
+      return false
+    end
+  end
+  return true
+end
